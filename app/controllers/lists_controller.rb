f5ff261ca012d1de12baa6c79f351c4896a53bda
Bodil Hundevad <bodil@hundevad.de>
2022-08-18 18:13:52 +0800

    list features created

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,34 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :edit]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      redirect_to lists_new_path
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,5 +3,5 @@ class Movie < ApplicationRecord
   has_many :lists, through: :bookmarks
 
   validates :title, :overview, presence: true
-  validates :title, :overview, uniqueness: true
+  validates :title, uniqueness: true
 end
